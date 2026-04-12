4f6e1c9bb9884fb15a53f04a605f3c773fe21e5e
Overlander <anuragnandal@hotmail.com>
2026-04-12 14:10:20 -0400

    feat: implement follow/unfollow system and show follower count

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,7 +6,7 @@ userRouter.get("/:username", async (req, res) => {
   const { username } = req.params;
 
   try {
-    const selectedUser = await prisma.user.findUnique({
+    const profileUser = await prisma.user.findUnique({
       where: { username: username },
       include: {
         posts: {
@@ -20,17 +20,57 @@ userRouter.get("/:username", async (req, res) => {
           },
           orderBy: { createdAt: "desc" },
         },
+        followers: { select: { id: true } },
+        _count: { select: { followers: true, following: true } },
       },
     });
 
-    if (!selectedUser) {
+    if (!profileUser) {
       return res.status(404).send("User not found");
     }
 
-    res.render("profile", { profileUser: selectedUser });
+    const isFollowing = req.user
+      ? profileUser.followers.some((f) => f.id === req.user.id)
+      : false;
+
+    res.render("profile", { profileUser, isFollowing });
   } catch (error) {
     console.error(error);
     res.status(500).send("Server error");
+  }
+});
+
+userRouter.post("/:username/follow", async (req, res) => {
+  const { username } = req.params;
+
+  try {
+    await prisma.user.update({
+      where: { id: req.user.id },
+      data: {
+        following: { connect: { username: username } },
+      },
+    });
+    res.redirect(`/users/${username}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Unable to follow user");
+  }
+});
+
+userRouter.delete("/:username/follow", async (req, res) => {
+  const { username } = req.params;
+
+  try {
+    await prisma.user.update({
+      where: { id: req.user.id },
+      data: {
+        following: { disconnect: { username: username } },
+      },
+    });
+    res.redirect(`/users/${username}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Unable to unfollow user");
   }
 });
 
